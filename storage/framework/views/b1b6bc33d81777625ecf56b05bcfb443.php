d23380f2d133618ac5ec5ca74a8237141d5342fc
ferdyvalentino123 <151491010+ferdyvalentino123@users.noreply.github.com>
2024-11-24 15:59:42 +0700

    Add files via upload

---
+++ storage/framework/views/b1b6bc33d81777625ecf56b05bcfb443.php
@@ -0,0 +1,45 @@
+
+<?php $__env->startSection('content'); ?>
+
+    <div class="container mt-4">
+        <div class="row mb-0">
+            <div class="col-lg-9 col-xl-10">
+                <h4 class="mb-3"><?php echo e($pageTitle); ?></h4>
+            </div>
+            <div class="col-lg-3 col-xl-2">
+                <div class="d-grid gap-2">
+                    <a href="<?php echo e(route('employees.create')); ?>" class="btn" style="background-color: maroon; color:white">Create Employee</a>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="table-responsive border p-3 rounded-3">
+            <table class="table table-bordered mb-0">
+                <thead style="background-color: maroon; color: white;">
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Position</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody style="background-color: rgb(255, 188, 224); color: maroon">
+                    <?php $__currentLoopData = $employees; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $employee): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <tr>
+                            <td><?php echo e($employee->firstname); ?></td>
+                            <td><?php echo e($employee->lastname); ?></td>
+                            <td><?php echo e($employee->email); ?></td>
+                            <td><?php echo e($employee->age); ?></td>
+                            <td><?php echo e($employee->position->name); ?></td>
+                            <td><?php echo $__env->make('employee.action', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?></td>
+                        </tr>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\PraktikumGitKelompok\resources\views/employee/index.blade.php ENDPATH**/ ?>
